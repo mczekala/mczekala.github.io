f2a531bd777510f695194b87b122596350f51fef
Marcin Czekala <marcin_czekala@icloud.com>
2018-11-27 20:16:14 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
-
 $(document).ready(function () {
     var navbar = document.getElementsByTagName("nav")[0];
     var sticky = navbar.offsetTop;
 
     function myFunction() {
-      if(window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-      } else {
-        navbar.classList.remove("sticky");
-      }
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky");
+        } else {
+            navbar.classList.remove("sticky");
+        }
     }
 
-    window.onscroll = function() { myFunction(); };
-
-    var slideCenter = $(window).width()/2-100;
-
-    window.addEventListener("scroll", function(event) {
-        $(".slide").animate({
-            marginLeft: slideCenter
-        },1000);
-    });          
+    window.onscroll = function () {
+        myFunction();
+    };
+    window.addEventListener("scroll", function (event) {
+        if (window.pageYOffset + window.innerHeight > $('.image-wrapper').position().top) {
+            $(".left-slide").animate({
+                left: 100
+            }, 500);
+            $(".right-slide").animate({
+                left: -100
+            }, 500);
+        } else {
+            $(".left-slide").stop(true, true).css("left", "-100vw");
+            $(".right-slide").stop(true, true).css("left", "100vw");
+        }
+    })
 }, false);
