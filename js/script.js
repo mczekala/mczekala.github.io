4db18d1dbd94d1795c409f0fe932a144c368f889
Marcin Czekala <marcin_czekala@icloud.com>
2018-11-28 14:21:48 +0100

    fix header

--- js/script.js
+++ js/script.js
@@ -26,4 +26,14 @@ $(document).ready(function () {
             $(".right-slide").stop(true, true).css("left", "100vw");
         }
     })
+
+
+    $(function () {
+        $('a[href*=#]').on('click', function (e) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top
+            }, 500, 'linear');
+        });
+    });
 }, false);
